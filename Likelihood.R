56c89844b17196e64c4fb1670d6e2515c8269313
Daniele Tramontano <88477195+danieletramontano@users.noreply.github.com>
2022-09-16 17:09:42 +0200

    All the likelihood tests

--- Likelihood.R
+++ Likelihood.R
@@ -23,9 +23,6 @@ pairlikelihood<-function(Xlist,Ilist,Nlist,u,v){
   }
   
   
-  NN<-sum(Nlist[notvlist[]])
-  MLAMBDA<-0
-  
   polys<-list()
   derpolys<-list()
   for(i in c(1:length(notvlist))){
@@ -33,7 +30,6 @@ pairlikelihood<-function(Xlist,Ilist,Nlist,u,v){
     b<-(Xlist[[notvlist[i]]][,u]%*%Xlist[[notvlist[i]]][,v])
     c<-(Xlist[[notvlist[i]]][,v]%*%Xlist[[notvlist[i]]][,v])
     
-    MLAMBDA<-MLAMBDA+(Nlist[notvlist[i]]/NN)*(b/a)
     polys[[i]]<-mp(paste0(a," l l-",2*b," l+",c))
     derpolys[[i]]<-deriv(polys[[i]],"l")
   }
@@ -59,7 +55,6 @@ pairlikelihood<-function(Xlist,Ilist,Nlist,u,v){
   SOL<-optim(0,method="Brent",llike,derllike,lower=-1,upper=1)
   
   maxloglik<-SOL$value
-  Par<-SOL$par
   
   if(length(vlist)>0){
     for(i in c(1:length(vlist))){
@@ -74,7 +69,7 @@ pairlikelihood<-function(Xlist,Ilist,Nlist,u,v){
   for(i in c(1:l)){
     maxloglik<-maxloglik+(Nlist[i]/2)*log((Xlist[[i]][,u]%*%Xlist[[i]][,u]))
   }
-  return(list(ML=maxloglik,NVL=notvlist,VL=vlist,PAR=Par,Mlambda=MLAMBDA))
+  return(-maxloglik)
 }
 
 
@@ -315,3 +310,233 @@ dir_ident<-function(Ilist,E){
   return(d)
 }
 
+#Function that computes that connected component of the node "u" in the unoriented part of 
+#the CPDAG
+#INPUT:   E= list of unoriented edges
+#         u= index of the node
+#OUTPUT:  E_u= list of edges in the connencted component
+connected_components<-function(E,cE,E_u,u){
+  len<-length(E[,1])
+  list_u<-which(E==u)
+  row_u<-col_u<-c()
+  for(i in c(1:length(list_u))){
+    row_u[i]<-list_u[i]%%l
+    col_u[i]<-list_u[i]%/%(l+1)+1
+    if(row_u[i]==0){
+      if(col_u[i]==1){
+        row_i[i]<-l
+      }else{
+        row_i[i]<-2*l
+      }
+    }
+  }
+  cE<-rbind(cE,E[row_u,])
+  E_u<-E[row_u,]
+  vert_list<-E_u[,(3-col_u)]
+  E<-E[-row_u,]
+  for(i in c(1:length(list_u))){
+    connected_components()
+  }
+  return(E_u)
+}
+
+
+#Function that computes an identifiable connected component after the collider search
+#INPUT:   E= list of unoriented edges
+#         IDvertices= list of possible root vertices
+#OUTPUT:  unoriented= list of still unoriented edges
+#         tboriented= list of edges in the connected component that has to be oriented
+
+
+i_component<-function(E,IDvertices){
+  
+  G<-graph_from_edgelist(E,directed=FALSE)
+  v_list<-c(1:max(E))
+  V(G)$name<-v_list
+  clusters_G<-components(G)
+  members<-clusters_G$membership
+  c_size<-clusters_G$csize
+  
+  check<-FALSE
+  for(i in c(1:length(IDvertices))){
+    ind_int<-which(v_list==IDvertices[i])
+    if(check==FALSE){
+      if(length(ind_int)>0){
+        cluster_ind<-members[ind_int]
+        if(c_size[ind_int]>1){
+          id_component<-which(members==ind_int)
+          check<-TRUE
+        }
+      }
+    }
+  }
+  
+  G_sub<-induced_subgraph(G,id_component)
+  E_sub<-get.edgelist(G_sub,names=TRUE)
+  
+  G_comp<-induced_subgraph(G,v_list[-id_component])
+  E<-get.edgelist(G_comp)
+  
+  return(list(unoriented=E,tboriented=E_sub))
+}
+
+
+
+#Finds the maximum likelihood root as in 4.2
+#INPUT:   Xlist=list of samples
+#         Ilist=list of intervened nodes
+#         Nlist=list of sample sizes
+#         E= list of edges in the connected component
+#         IDvertices= list of possible roots in the connected component
+aftercollider_test<-function(Xlist,Ilist,Nlist,E,IDvertices){
+  G<-graph_from_edgelist(E,directed=FALSE)
+  v_list<-V(G)
+  likelihood_matrix<-matrix(0,nrow=length(v_list),ncol=length(v_list))
+  
+  for(i in c(1:length(E[,1]))){
+    u<-E[i,1]
+    v<-E[i,2]
+    likelihood_matrix[u,v]<-aftercollider_condlikelihood(Xlist,Ilist,Nlist,u,v)
+    likelihood_matrix[v,u]<-aftercollider_condlikelihood(Xlist,Ilist,Nlist,v,u)
+    if(likelihood_matrix[u,u]==0){
+      likelihood_matrix[u,u]<-aftercollider_marginallikelihood(Xlist,Ilist,Nlist,u)
+    }
+    if(likelihood_matrix[v,v]==0){
+      likelihood_matrix[v,v]<-aftercollider_marginallikelihood(Xlist,Ilist,Nlist,v)
+    }
+  }
+  
+  ll<-c()
+  for(i in c(1:length(IDvertices))){
+    ll[i]<-root_likelihood(0,likelihood_matrix,IDvertices[i],c(),G)
+    ll[i]<-ll[i]+likelihood_matrix[IDvertices[i],IDvertices[i]]
+  }
+  return(IDvertices(which.max(ll)))
+}
+
+
+#Computes the marginal maximum likelihood of v after the collider search 
+#as in 4.2 of the paper
+#INPUT:   Xlist=list of samples
+#         Ilist=list of intervened nodes
+#         Nlist=list of sample sizes
+#         v= index of the node
+#OUTPUT:  marginal maximum likelihood of v
+aftercollider_marginallikelihood<-function(Xlist,Ilist,Nlist,v){
+  l<-length(Ilist)
+  notvlist<-c()
+  vlist<-c()
+  
+  for(i in c(1:l)){
+    if(length(intersect(Ilist[[i]][-1],v))==0){
+      notvlist<-append(notvlist,i)
+    }
+    else{
+      vlist<-append(vlist,i)
+    }
+  }
+  
+  NN<-sum(Nlist[notvlist[]])
+  
+  i_variance<-0
+  for(i in c(1:length(notvlist))){
+    i_variance<-i_variance+Xlist[[notvlist[i]]][,v]%*%Xlist[[notvlist[i]]][,v]
+  }
+  maxloglik<- (-1/NN)*log(i_variance)
+  
+  for(i in c(1:lenght(vlist))){
+    maxloglik<-maxloglik-(Nlist[i]/2)*log((Xlist[[i]][,v]%*%Xlist[[i]][,v]))
+  }
+  return(maxloglik)
+}
+
+
+#Computes the conditional maximum likelihood of v given u after the collider search 
+#as in 4.2 of the paper
+#INPUT:   Xlist=list of samples
+#         Ilist=list of intervened nodes
+#         Nlist=list of sample sizes
+#         u,v= indices of the nodes
+#OUTPUT:  conditional maximum likelihood of v given u
+aftercollider_condlikelihood<-function(Xlist,Ilist,Nlist,u,v){
+  
+  l<-length(Ilist)
+  notvlist<-c()
+  vlist<-c()
+  
+  for(i in c(1:l)){
+    if(length(intersect(Ilist[[i]][-1],v))==0){
+      notvlist<-append(notvlist,i)
+    }
+    else{
+      vlist<-append(vlist,i)
+    }
+  }
+  
+  NN<-sum(Nlist[notvlist[]])
+
+  polys<-list()
+  i_covariance<-0
+  i_length<-0
+
+  for(i in c(1:length(notvlist))){
+    a<-(Xlist[[notvlist[i]]][,u]%*%Xlist[[notvlist[i]]][,u])
+    b<-(Xlist[[notvlist[i]]][,u]%*%Xlist[[notvlist[i]]][,v])
+    c<-(Xlist[[notvlist[i]]][,v]%*%Xlist[[notvlist[i]]][,v])
+    i_covariance<-i_covariance+b
+    i_length<-i_length+a
+    
+    polys[[i]]<-mp(paste0(a," l l-",2*b," l+",c))
+  }
+  
+  i_reg_coef<-i_covariance/i_length
+  
+  i_residual<-0
+  
+  for(i in c(1:length(notvlist))){
+    i_residual<-i_residual+as.function(polys[[i]])(i_reg_coef)
+  }
+  
+  maxloglik<- (NN/2)*log(i_residual)
+  
+  
+  if(length(vlist)>0){
+    for(i in c(1:length(vlist))){
+      
+      a<-(Xlist[[vlist[i]]][,u]%*%Xlist[[vlist[i]]][,u])
+      b<-(Xlist[[vlist[i]]][,u]%*%Xlist[[vlist[i]]][,v])
+      c<-(Xlist[[vlist[i]]][,v]%*%Xlist[[vlist[i]]][,v])
+      
+      maxloglik<-maxloglik-(Nlist[vlist[i]]/2)*log(as.function(mp(paste0(a," l l-",2*b," l+",c)))(b/a))
+    }
+  }
+  
+  return(maxloglik)
+
+}
+
+
+#Recursive function that computes the maximum likelihood of the model in which r is the root
+#Base case input:   partial_lik=0, 
+#                   L=likelihood_matrix as defined in aftercollider_test
+#                   r= index of the root
+#                   not_neigh= c()
+#                   G= subgraph as defined in aftercollider_test
+#Output:            maximum likelihood of the model
+root_likelihood<-function(partial_lik,L,r,not_neigh,G){
+  r_neigh<-neighbors(G,r)
+  not_neigh_index<-which(r_neigh==not_neigh)
+  if(length(not_neigh)>0){
+    if(length(r_neigh)>1){
+      r_neigh<-r_neigh[-not_neigh_index]
+    }
+    else{
+      return(partial_lik)
+    }
+  }
+  for(i in c(1:length(r_neigh))){
+    partial_lik<-partial_lik+L[r,r_neigh[i]]
+    partial_lik<-root_likelihood(partial_lik,L,r_neigh[i],r,G)
+  }
+  return(partial_lik)
+}
