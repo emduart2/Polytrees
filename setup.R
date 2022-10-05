967ddc9f2cfdb9194bb1b6f4646da3b1a8c45884
LeWaldm <leonard.waldmann@gmx.de>
2022-10-05 16:41:36 +0200

    ready for cluster

--- setup.R
+++ setup.R
@@ -1,12 +1,28 @@
+# load custom functions
 source("polyFunctions.R")
 source("Likelihood.R")
-source("expl_skeleton.R")
-source("expl_orientation.R")
-source("expl_both.R")
+source("explore_performance.R")
+source("eval_fcts.R")
 library(pcalg)
 library(patchwork)
 library(igraph)
 library(sets)
+library(mpoly)
 
-# optional for plotting
-library(ggarrange)
+# setup parallel cores
+n.cores <- parallel::detectCores() - 1
+my.cluster <- parallel::makeCluster(
+  n.cores,
+  type = "FORK"
+)
+doParallel::registerDoParallel(cl = my.cluster)
+
+
+# execute main file
+s = Sys.time()
+source("largeScale_cluster.R")
+time_elapsed = Sys.time() - s
+
+
+# close parallel cores
+parallel::stopCluster(cl = my.cluster)
