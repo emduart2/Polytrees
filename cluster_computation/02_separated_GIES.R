54b0a41669043961c196f0878343fba7e10378d8
LeWaldm <leonard.waldmann@gmx.de>
2022-10-07 12:42:40 +0200

    added new cluster script

---
+++ cluster_computation/02_separated_GIES.R
@@ -0,0 +1,339 @@
+# notes: 
+# - skeletonExploration now includes baseline plots (hence replot fig1)
+# - high-dim: 1000 nodes, 21 datasets (2%), interventionsize 1
+kmax = 50
+save_dir = 'testres/'  # with final '/' in the end
+experiment_id = "02"
+
+
+#---- fig 1 but high-dim ----
+# only take one of totalSamples for figure middle&right
+# left
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore_skeleton(df_params)
+saveRDS(l, file = paste(save_dir,experiment_id,"-04.Rds",sep=''))
+
+# middle
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(11,21,31),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore_skeleton(df_params); 
+saveRDS(l, file = paste(save_dir,experiment_id,"-05.Rds",sep=''))
+
+# right
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1,5,10),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore_skeleton(df_params);
+saveRDS(l, file = paste(save_dir,experiment_id,"-06.Rds",sep=''))
+
+
+
+
+#---- fig 2 but high-dim ----
+# - for left, only take one of totalSamples
+# - compare BIC vs TEST (also on runtime!)
+# left
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(2),
+  ndatasets = c(11,21,31),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","1simp"),c("mean","2"),c("mean","2simp"),c("mean","3"),c("mean","3simp")),
+  pw_methods_all = c("BIC","TEST"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-07.Rds",sep=''))
+
+# right
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(2),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","1simp"),c("mean","2"),c("mean","2simp"),c("mean","3"),c("mean","3simp")),
+  pw_methods_all = c("BIC","TEST"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-08.Rds",sep=''))
+
+
+
+#---- fig 2 but with all intervention types ----
+# left
+df_params <- expand.grid(
+  tsize = c(20),
+  totalSamples = c(500),
+  interventionSize = c(2),
+  ndatasets = c(2,11,21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect","imperfect","inhibitory","random"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","1simp"),c("mean","2"),c("mean","2simp"),c("mean","3"),c("mean","3simp")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-09.Rds",sep=''))
+
+# right
+df_params <- expand.grid(
+  tsize = c(20),
+  totalSamples = c(100,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect","imperfect","inhibitory","random"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","1simp"),c("mean","2"),c("mean","2simp"),c("mean","3"),c("mean","3simp")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-10.Rds",sep=''))
+
+
+#---- potential fig 3: compare GIES to our algo in high-dim for varying nsamples ----
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","3")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-11.Rds",sep=''))
+
+# same but only GIES
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("GIES","GIES")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-11_GIES.Rds",sep=''))
+
+
+
+#---- potential fig 3: compare GIES to our algo in high-dim for varying ndatasets ----
+# pick one samplesize
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(11,21,31),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","3")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-12.Rds",sep=''))
+
+# same but only GIES
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(11,21,31),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = FALSE,
+  dag_nbh = 0
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives), 
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("GIES","GIES")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-12_GIES.Rds",sep=''))
+
+
+
+# #---- potential fig 3: high-dim DAG setting for varying nsamples ----
+# nbh = 1
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = TRUE,
+  dag_nbh = 1
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives),
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","3")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-13.Rds",sep=''))
+
+
+# nbh = 1, GIES
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(200,500,1000),
+  interventionSize = c(1),
+  ndatasets = c(21),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = TRUE,
+  dag_nbh = 1
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives),
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("GIES","GIES")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-13_GIES.Rds",sep=''))
+
+
+# nbh = 3
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(500),
+  interventionSize = c(1),
+  ndatasets = c(11,31),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = TRUE,
+  dag_nbh = 3
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives),
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("mean","1"),c("mean","3")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-14.Rds",sep=''))
+
+# nbh = 3, GIES
+df_params <- expand.grid(
+  tsize = c(1000),
+  totalSamples = c(500),
+  interventionSize = c(1),
+  ndatasets = c(11,31),
+  k = c(1:kmax),
+  sdatasets = list(c()),
+  kindOfIntervention = c("perfect"),
+  ensureDiff = TRUE,
+  alpha = 0.05,
+  use_dags = TRUE,
+  dag_nbh = 3
+)
+l <- explore(
+  df_params,
+  scoreFct_all = list(pcalg::shd, SID, true_positives, false_positives, true_negatives, false_negatives),
+  sFctNames_all = c("SHD","SID","TP","FP","TN","FN"),
+  methods_all = list(c("GIES","GIES")),
+  pw_methods_all = c("BIC"))
+saveRDS(l, file = paste(save_dir,experiment_id,"-14_GIES.Rds",sep=''))
