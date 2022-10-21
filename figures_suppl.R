752f5ee2e160c364ad3b235612f2ae09f477602c
LeWaldm <leonard.waldmann@gmx.de>
2022-10-21 11:07:01 +0200

    sorted figures to align the ones in supplement

--- figures_suppl.R
+++ figures_suppl.R
@@ -198,9 +198,58 @@ colors_ort = c(
 
 
 
+# ---- fig2: skel, pooled data (LD) ----
+data_folder = "data/"
+df = readRDS(paste(data_folder, "supp_pooled.Rds",sep=""))$df
+df = df[df$interventionSize %in% c(1,6,10),]
+df = pretty_names(df)
+df$method = factor(df$method, levels = c("baseObs","ltest","mean","median","pooled"))
+
+capitalize <- function(string) {
+  substr(string, 1, 1) <- toupper(substr(string, 1, 1))
+  string
+}
+fig_pooled = ggplot(df, aes(df[,x],SHD,fill=factor(method))) + geom_boxplot() +
+  facet_grid(cols=ggplot2::vars(kindOfIntervention), labeller=labeller(.default = capitalize))+
+  xlab(dict_axisLabs(x)) + labs(title="p=20, n=500, d=21",fill="Aggr. Fct.") +
+  theme(legend.position = "right", plot.title = element_text(face="bold",hjust=0.5)) +
+  scale_fill_manual(values = colors_skel) + 
+  theme(strip.text.x = element_text(size = 12))
+fig_pooled
 
 
-#---- ort: main analysis -----
+
+
+# ---- fig3: skel, runtime  ----
+data_folder = "data/"  # with final '/'
+y = "time_s"
+aggrFcts = c("ltest","mean","median")
+dfAll = rbind(hd_skel1, hd_skel2, hd_skel3)
+ylimits = c(min(dfAll[,y],na.rm=TRUE), max(dfAll[,y],na.rm=TRUE))
+hd_skel1 = readRDS(paste(data_folder, "1a_01_runtime.Rds",sep=""))
+hd_skel1 = hd_skel1$df[hd_skel1$df$method %in% aggrFcts, ]
+f1 = ggplot(hd_skel1, aes(totalSamples, hd_skel1[,y], fill=factor(method))) + geom_boxplot() + 
+  labs(fill="Aggr. fct.", title = "p=500, d=20, k=1") + 
+  xlab(dict_axisLabs("totalSamples")) + ylab(dict_axisLabs(y)) + ylim(ylimits) +
+  scale_fill_manual(values = colors_skel, breaks = aggrFcts)
+hd_skel2 = readRDS(paste(data_folder, "1a_02_runtime.Rds",sep=""))
+hd_skel2 = hd_skel2$df[hd_skel2$df$method %in% aggrFcts, ]
+f2 = ggplot(hd_skel2, aes(ndatasets, time_s, fill=factor(method))) + geom_boxplot() + 
+  labs(fill="Aggr. fct.", title = "p=500, n=500, k=1") + 
+  xlab(dict_axisLabs("ndatasets")) + ylab(dict_axisLabs(y))+ ylim(ylimits) +
+  scale_fill_manual(values = colors_skel, breaks = aggrFcts)
+hd_skel3 = readRDS(paste(data_folder, "1a_03_runtime.Rds",sep=""))
+hd_skel3 = hd_skel3$df[hd_skel3$df$method %in% aggrFcts, ]
+f3 = ggplot(hd_skel3, aes(interventionSize, time_s, fill=factor(method))) + geom_boxplot() + 
+  labs(fill="Aggr. fct.", title = "p=500, n=500, d=20") + 
+  xlab(dict_axisLabs("interventionSize")) + ylab(dict_axisLabs(y))+ ylim(ylimits)+
+  scale_fill_manual(values = colors_skel, breaks = aggrFcts)
+fig1a_runtime = f1 + f2 + f3 + plot_layout(nrow = 1, guides="collect") & 
+  theme(legend.position = "right", plot.title = element_text(face="bold"))
+fig1a_runtime
+
+
+# ---- fig4: ort, main analysis -----
 l_nds = readRDS("data_new/Orientation/03-01.Rds") #ndatasets
 l_smpls = readRDS("data_new/Orientation/03-02.Rds"); # totalsamples
 l_intvS = readRDS("data_new/Orientation/03-03.Rds");  # intervSize
@@ -231,169 +280,7 @@ fig3
 
 
 
-# ---- dags: different neighborhoods ----
-# load_dir = 'old_cluster_comp/'
-# l6 = readRDS(paste(load_dir,"05-06.Rds",sep=''))
-# l6_GIES = readRDS(paste(load_dir,"05-06_GIES.Rds",sep=''))
-# nbh1 = rbind(l6$df, l6_GIES$df)
-# nbh1 = rbind(nbh1)
-# l7 = readRDS(paste(load_dir,"05-07.Rds",sep=''))
-# l7_GIES = readRDS(paste(load_dir,"05-07_GIES.Rds",sep=''))
-# nbh5 = rbind(l7$df, l7_GIES$df)
-# nbh5 = rbind(nbh5)
-# l8 = readRDS(paste(load_dir,"05-08.Rds",sep=''))
-# nbh10 = rbind(l8$df)
-# 
-# 
-# dfAll = rbind(nbh1, nbh5, nbh10)
-# y = "SHD"
-# means = c("1" = 1000, "5" =2000, "10"=3000)
-# ggplot(dfAll, aes(totalSamples, dfAll[,y], fill=method)) + geom_boxplot() + ylab(y)+
-#   geom_hline(aes(yintercept = value), data=means) +  
-#   facet_grid(cols = ggplot2::vars(dag_nbh), labeller = labeller(.default = label_both))
-# 
-# ylims = c(min(dfAll$SHD), max(dfAll$SHD))
-# nbh1_rand = mean(nbh1[nbh1$method == "shd_rand","SHD"])
-# nbh1_plt = nbh1[ !(nbh1$method %in% c("shd_opt","shd_rand")),]
-# # nbh1_plt = pretty_names(nbh1_plt)
-# fnbh1 = ggplot(nbh1_plt, aes(totalSamples, nbh1_plt[,y], fill=factor(method))) + geom_boxplot()+
-#   xlab(dict_axisLabs("totalSamples")) + ylab(dict_axisLabs(y)) + ylim(ylims)+
-#   geom_hline(yintercept = nbh1_rand, linetype = "dotted") + 
-#   labs(title = "p=500, d=21, k=10, h=1")
-# nbh5_rand = mean(nbh5[nbh5$method == "shd_rand","SHD"])
-# nbh5_plt = nbh5[ !(nbh5$method %in% c("shd_opt","shd_rand")),]
-# # nbh5_plt = pretty_names(nbh5_plt)
-# fnbh5 = ggplot(nbh5_plt, aes(totalSamples, nbh5_plt[,y], fill=factor(method))) + geom_boxplot()+
-#   xlab(dict_axisLabs("totalSamples")) + ylab(dict_axisLabs(y)) +ylim(ylims)+
-#   geom_hline(yintercept = nbh5_rand, linetype = "dotted") + 
-#   labs(title = "p=500, d=21, k=10, h=5")
-# nbh10_rand = mean(nbh10[nbh10$method == "shd_rand","SHD"])
-# nbh10_plt = nbh10[ !(nbh10$method %in% c("shd_opt","shd_rand")),]
-# # nbh10_plt = pretty_names(nbh10_plt)
-# # nbh10_plt$method = factor(nbh10_plt$method, levels = c("GIES","P.1,simple,IRC","P.2,simple,IRC"))
-# fnbh10 = ggplot(nbh10_plt, aes(totalSamples, nbh10_plt[,y], fill=factor(method))) + geom_boxplot()+
-#   xlab(dict_axisLabs("totalSamples")) + ylab(dict_axisLabs(y)) +ylim(ylims)+
-#   geom_hline(yintercept = nbh10_rand, linetype = "dotted") + 
-#   labs(title = "p=500, d=21, k=10, h=10")
-# fnbhs = fnbh1 + fnbh5 + fnbh10 + plot_layout(nrow=1, guides="collect") &
-#   theme(plot.title = element_text(face="bold"))
-# fnbhs
-# 
-# 
-# 
-# # all mean,3simp,TEST
-# l = readRDS("data_new/Results/05-vlarge_011.Rds"); unq(l$df) # PT: 1000 nodes, totalSamples
-# l = readRDS("data_new/Results/05-vlarge_012.Rds"); unq(l$df) # DAG: 1000 nodes, nbh=1,5,10,20
-#   # vlarge for GIES did not finish
-#   #  ATM: we are running vlarge with GIES
-# 
-# l = readRDS("data_new/Results/05-large_011.Rds"); unq(l$df) # DAG, 1000 nodes, 20 nbh
-# # l = readRDS("data_new/Results/05-large_022.Rds"); unq(l$df) # DAG: 2000 nodes, 20 nbh
-# 
-# 
-# tmp = l$df
-# tmp$method = "SHD_rand"
-# tmp$SHD = tmp$shd_rand
-# tmp2 = l$df
-# tmp2$method = "SHD_opt"
-# tmp2$SHD = tmp2$SHD_optimal
-# dfAll = rbind(l$df, tmp, tmp2)
-# dfAll$method = factor(dfAll$method, levels=c("SHD_rand","mean,3simp,TEST","SHD_opt"))
-# ggplot(dfAll, aes(factor(dag_nbh), SHD, fill=factor(method))) + geom_boxplot()
-# 
-
-
-# actual plotting
-randDag = function(dfAll, opt=FALSE){
-  df_randSHD = dfAll[dfAll$method == dfAll$method[1],] # select all entries from one method to get all simulated graphs exactly once
-  df_randSHD$method = "shd_rand"
-  df_randSHD$SHD = df_randSHD$shd_rand
-  if(opt){
-    df_opt = df_randSHD
-    df_opt$method = "shd_opt"
-    df_opt$SHD = df_opt$SHD_opt
-    df_randSHD = rbind(df_opt,df_randSHD)
-  }
-  return(df_randSHD)
-}
-l1 = readRDS("data_new/gies_comp/1-large_GIES_1.Rds")$df;  # pt, 
-l2 = readRDS("data_new/gies_comp/1-large_GIES_3.Rds")$df; # dags, nbh5, GIES
-l3 = readRDS("data_new/gies_comp/2-large_GIES_nb_1.Rds")$df;  # dags, nbh1, 
-l4 = readRDS("data_new/gies_comp/2-large_GIES_nb_10.Rds")$df;  # dags, nbh10
-l5 = readRDS("data_new/gies_comp/ours.Rds"); 
-ours = l5[[1]]$df
-ours = ours[ours$dag_nbh %in% c(1,5,10,20),]
-
-orderMethods = c("SHD rand.","GIES","P.2, simple, IRC")
-dfNbhs = rbind(l2,randDag(l2),l3,randDag(l3),l4,randDag(l4),ours, 
-               randDag(ours[ours$dag_nbh == 20,]))
-dfNbhs = pretty_names(dfNbhs)
-dfNbhs$method = factor(dfNbhs$method, levels=orderMethods)
-ggplot(dfNbhs, aes(factor(dag_nbh), SHD, fill=(method))) + geom_boxplot() +
-  scale_fill_manual(values=colors_ort, breaks=orderMethods) + 
-  labs(title="DAGs with p=500, n=1000, d=21, k=10", fill="Method") +
-  theme(plot.title = element_text(face="bold")) +
-  xlab(dict_axisLabs("dag_nbh")) + coord_trans(y="log10")
-
-
-
-
-
-
-
-# ---- skel: runtime  ----
-data_folder = "data/"  # with final '/'
-y = "time_s"
-aggrFcts = c("ltest","mean","median")
-dfAll = rbind(hd_skel1, hd_skel2, hd_skel3)
-ylimits = c(min(dfAll[,y],na.rm=TRUE), max(dfAll[,y],na.rm=TRUE))
-hd_skel1 = readRDS(paste(data_folder, "1a_01_runtime.Rds",sep=""))
-hd_skel1 = hd_skel1$df[hd_skel1$df$method %in% aggrFcts, ]
-f1 = ggplot(hd_skel1, aes(totalSamples, hd_skel1[,y], fill=factor(method))) + geom_boxplot() + 
-  labs(fill="Aggr. fct.", title = "p=500, d=20, k=1") + 
-  xlab(dict_axisLabs("totalSamples")) + ylab(dict_axisLabs(y)) + ylim(ylimits) +
-  scale_fill_manual(values = colors_skel, breaks = aggrFcts)
-hd_skel2 = readRDS(paste(data_folder, "1a_02_runtime.Rds",sep=""))
-hd_skel2 = hd_skel2$df[hd_skel2$df$method %in% aggrFcts, ]
-f2 = ggplot(hd_skel2, aes(ndatasets, time_s, fill=factor(method))) + geom_boxplot() + 
-  labs(fill="Aggr. fct.", title = "p=500, n=500, k=1") + 
-  xlab(dict_axisLabs("ndatasets")) + ylab(dict_axisLabs(y))+ ylim(ylimits) +
-  scale_fill_manual(values = colors_skel, breaks = aggrFcts)
-hd_skel3 = readRDS(paste(data_folder, "1a_03_runtime.Rds",sep=""))
-hd_skel3 = hd_skel3$df[hd_skel3$df$method %in% aggrFcts, ]
-f3 = ggplot(hd_skel3, aes(interventionSize, time_s, fill=factor(method))) + geom_boxplot() + 
-  labs(fill="Aggr. fct.", title = "p=500, n=500, d=20") + 
-  xlab(dict_axisLabs("interventionSize")) + ylab(dict_axisLabs(y))+ ylim(ylimits)+
-  scale_fill_manual(values = colors_skel, breaks = aggrFcts)
-fig1a_runtime = f1 + f2 + f3 + plot_layout(nrow = 1, guides="collect") & 
-  theme(legend.position = "right", plot.title = element_text(face="bold"))
-fig1a_runtime
-
-
-# ---- skel: pooled data ----
-# low dimensional
-df = readRDS(paste(data_folder, "supp_pooled.Rds",sep=""))$df
-df = df[df$interventionSize %in% c(1,6,10),]
-df = pretty_names(df)
-df$method = factor(df$method, levels = c("baseObs","ltest","mean","median","pooled"))
-
-capitalize <- function(string) {
-  substr(string, 1, 1) <- toupper(substr(string, 1, 1))
-  string
-}
-fig_pooled = ggplot(df, aes(df[,x],SHD,fill=factor(method))) + geom_boxplot() +
-  facet_grid(cols=ggplot2::vars(kindOfIntervention), labeller=labeller(.default = capitalize))+
-  xlab(dict_axisLabs(x)) + labs(title="p=20, n=500, d=21",fill="Aggr. Fct.") +
-  theme(legend.position = "right", plot.title = element_text(face="bold",hjust=0.5)) +
-  scale_fill_manual(values = colors_skel) + 
-  theme(strip.text.x = element_text(size = 12))
-fig_pooled
-
-
-
-
-
-# ---- skel: different intervention types ----
+# ---- fig5: skel, different intervention types ----
 # load and process data
 data_folder = 'data/'
 nds_diff = readRDS("data_new/skeleton_recovery/skeleton_01.Rds")$df # ndatasets
@@ -451,7 +338,8 @@ fig_skel_intv
 
 
 
-#---- ort: diff intervention types ----
+
+# ---- fig6: ort, diff intervention types ----
 # load low-dim data
 titles = c("p=20, d=11, k=2","p=20, n=500, k=2", "p=20, n=500, d=11")
 ort_nds = readRDS("data_new/Orientation/ld_03-01.Rds")$df; unq(ld1) # nds
@@ -510,7 +398,46 @@ fig_ort_intv
 
 
 
-#---- unbalanced sample sizes ----
+
+# ---- fig7: dags, different neighborhoods ----
+randomDag = function(dfAll, opt=FALSE){
+  df_randSHD = dfAll[dfAll$method == dfAll$method[1],] # select all entries from one method to get all simulated graphs exactly once
+  df_randSHD$method = "shd_rand"
+  df_randSHD$SHD = df_randSHD$shd_rand
+  if(opt){
+    df_opt = df_randSHD
+    df_opt$method = "shd_opt"
+    df_opt$SHD = df_opt$SHD_opt
+    df_randSHD = rbind(df_opt,df_randSHD)
+  }
+  return(df_randSHD)
+}
+l1 = readRDS("data_new/gies_comp/1-large_GIES_1.Rds")$df;  # pt, 
+l2 = readRDS("data_new/gies_comp/1-large_GIES_3.Rds")$df; # dags, nbh5, GIES
+l3 = readRDS("data_new/gies_comp/2-large_GIES_nb_1.Rds")$df;  # dags, nbh1, 
+l4 = readRDS("data_new/gies_comp/2-large_GIES_nb_10.Rds")$df;  # dags, nbh10
+l5 = readRDS("data_new/gies_comp/ours.Rds"); 
+ours = l5[[1]]$df
+ours = ours[ours$dag_nbh %in% c(1,5,10,20),]
+
+orderMethods = c("SHD rand.","GIES","P.2, simple, IRC")
+dfNbhs = rbind(l2,randomDag(l2),l3,randomDag(l3),l4,randomDag(l4),ours, 
+               randomDag(ours[ours$dag_nbh == 20,]))
+dfNbhs = pretty_names(dfNbhs)
+dfNbhs$method = factor(dfNbhs$method, levels=orderMethods)
+ggplot(dfNbhs, aes(factor(dag_nbh), SHD, fill=(method))) + geom_boxplot() +
+  scale_fill_manual(values=colors_ort, breaks=orderMethods) + 
+  labs(title="DAGs with p=500, n=1000, d=21, k=10", fill="Method") +
+  theme(plot.title = element_text(face="bold")) +
+  xlab(dict_axisLabs("dag_nbh")) + coord_trans(y="log10")
+
+
+
+
+
+
+
+# ---- fig8&9: unbalanced sample sizes ----
 load("data_new/unbalanced(2)/unbalanceddata.RData")
 
 # Skeleton
